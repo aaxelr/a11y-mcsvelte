341facf5b551b9fbc2ccf4c6a67977a3d4037f01
aaxelr <antonio.axelsson@gmail.com>
2022-06-02 15:20:56 +0200

    add TextInput

---
+++ src/lib/components/TextInput/TextInput.stories.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import TextInput from './TextInput.svelte';
+</script>
+
+<Meta
+  title="Input Components/Text Input"
+  component={TextInput}
+  argTypes={{
+    labelText: { defaultValue: 'Enter test string:', type: 'string', description: 'Label text' },
+    labelTextColor: {
+      defaultValue: 'black',
+      type: 'string',
+      description: 'Label text color',
+    },
+    value: { defaultValue: '', type: 'string', description: 'Text input field value' },
+    fullWidth: { defaultValue: false, control: 'boolean', description: 'Full width?' },
+  }}
+/>
+
+<Template let:args>
+  <TextInput {...args} />
+</Template>
+
+<Story name="Standard" />
+
+<Story
+  name="Full-Width"
+  args={{
+    fullWidth: true,
+  }}
+/>
---
+++ src/lib/components/TextInput/TextInput.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  // TODO: Break out common props
+  export let labelText: string;
+  export let labelTextColor: string = 'black';
+  export let fullWidth: boolean;
+  export let value: string;
+</script>
+
+<div>
+  <label style='color: {labelTextColor}'>
+    {labelText}
+    <input type="text" bind:value class={fullWidth ? 'full-width' : ''} />
+  </label>
+</div>
+
+<style>
+  label {
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 1.2em;
+  }
+  .full-width {
+    width: 100%;
+  }
+</style>
