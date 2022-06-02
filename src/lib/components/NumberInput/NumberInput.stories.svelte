7cf0bcb0890e271ec5b9d7c76efc68cc6ca2512f
aaxelr <antonio.axelsson@gmail.com>
2022-06-02 15:21:18 +0200

    add NumberInput

---
+++ src/lib/components/NumberInput/NumberInput.stories.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import NumberInput from './NumberInput.svelte';
+</script>
+
+<Meta
+  title="Input Components/Number Input"
+  component={NumberInput}
+  argTypes={{
+    labelText: { defaultValue: 'Enter test number:', type: 'string', description: 'Label text' },
+    labelTextColor: {
+      defaultValue: 'black',
+      type: 'string',
+      description: 'Label text color',
+    },
+    value: { defaultValue: '', type: 'number', description: 'Number input field value' },
+    fullWidth: { defaultValue: false, control: 'boolean', description: 'Full Width?' },
+    step: { control: 'number', description: 'Step value' },
+    minValue: { control: 'number', description: 'Minimum value' },
+    maxValue: { control: 'number', description: 'Maximum value' },
+  }}
+/>
+
+<Template let:args>
+  <NumberInput {...args} />
+</Template>
+
+<Story name="Standard" args={{}} />
+
+<Story
+  name="Full-Width"
+  args={{
+    fullWidth: true,
+  }}
+/>
---
+++ src/lib/components/NumberInput/NumberInput.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export let labelText: string;
+  export let labelTextColor: string = 'black';
+  export let fullWidth: boolean;
+  export let value: string;
+  export let minValue: number;
+  export let maxValue: number;
+  export let step: number = 1;
+</script>
+
+<div>
+  <label style='color: {labelTextColor}'>
+    {labelText}
+    <input
+      type="number"
+      bind:value
+      {step}
+      min={minValue}
+      max={maxValue}
+      class={fullWidth ? 'full-width' : ''}
+    />
+  </label>
+</div>
+
+<style>
+  label {
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 1.2em;
+  }
+  .full-width {
+    width: 100%;
+  }
+</style>
